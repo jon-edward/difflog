55c23271a50151b1a8c48c5a41d7db14784ee9df
jon-edward <townsend.jonathan.e@gmail.com>
2025-05-24 22:33:21 -0400

    Add check for in git repo

--- difflog/git_report.py
+++ difflog/git_report.py
@@ -12,6 +12,12 @@ from difflog.diff import ApiChange, diff
 
 __all__ = ("git_report", "md_report")
 
+def _check_in_git_repo():
+    try:
+        subprocess.check_output(["git", "rev-parse"], stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        raise RuntimeError("not in git repo")
+
 def _diff_content(changes: list[ApiChange]) -> str:
     return '\n'.join(sorted([change._diff_symbol + ' ' + change.describe() for change in changes]))
 
@@ -37,6 +43,8 @@ def _git_changed_files(from_rev: str | None = None, to_rev: str | None = None) -
     If from_rev is not None, returns the files changed since the given hash.
     Else, returns the files changed since the last push.
     """
+
+    _check_in_git_repo()
     
     if from_rev is None:
         # Get upstream revision if from_rev is not provided
@@ -55,6 +63,8 @@ def _git_changed_files(from_rev: str | None = None, to_rev: str | None = None) -
     return from_rev, to_rev, [line.strip() for line in output.splitlines() if line.strip()]
 
 def _git_content_from_file(file_path: str, rev: str | None) -> str:
+    _check_in_git_repo()
+
     try:
         if rev is None:
             # If rev is not provided, read the file from disk
